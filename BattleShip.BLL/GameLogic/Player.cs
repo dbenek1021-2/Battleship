5f9093318d57bfd599072eecbae286e2816dfae0
Danielle (Dani) Benek <dbenek1021@gmail.com>
2019-10-24 15:30:24 -0400

    Add files via upload

---
+++ BattleShip.BLL/GameLogic/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip.BLL.GameLogic
+{
+	public class Player
+	{
+		public string Name { get; set; }
+		public Board PlayerBoard;
+		public Board GuessBoard;
+		public bool WhoseTurn { get; set; }
+	}
+}
